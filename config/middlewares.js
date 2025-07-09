aeb5a4cab7446a19fb2118500f47b55c5fd97fef
Sudhan Kandel <sujan.kandel.0ne8@gmail.com>
2025-07-09 14:15:24 +0545

    efforts to make the website work

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,21 +1,12 @@
 module.exports = [
-  'strapi::errors',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ['https://e-comm-xi-eight.vercel.app'], // your frontend domain
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      headers: '*',
-      credentials: true,
-    },
-  },
-  'strapi::security',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
-
--- config/server.js
+++ config/server.js
@@ -1,23 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: env('RENDER_EXTERNAL_URL', 'https://artevo-backend.onrender.com'), // ✅ public URL of backend
-
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
-
-
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET'),
-    },
-    url: '/admin',
 });
-
-});
-
