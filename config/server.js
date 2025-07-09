9e923c90866729a50ecb8f6f729d0cb73e87f137
Sudhan Kandel <sujan.kandel.0ne8@gmail.com>
2025-07-09 09:42:59 +0545

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,10 +1,20 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('RENDER_EXTERNAL_URL', 'https://artevo-backend.onrender.com'), // ✅ public URL of backend
+
   app: {
     keys: env.array('APP_KEYS'),
   },
+
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+    url: '/admin', // optional, but safe to declare
   },
 });
